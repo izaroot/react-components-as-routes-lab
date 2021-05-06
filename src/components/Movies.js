f7c552d20cad8e318a3f6f64115fe3628d101f3e
Isaac <izaroot327@gmail.com>
2021-05-06 16:54:17 -0400

    Completed Assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movies => 
+            <div>
+              <h3>{movies.title}</h3>
+              Time: {movies.time}<br></br>
+              Genres:
+              <ul>
+                {movies.genres.map(genre => <li>{genre}</li>)}
+              </ul>
+            </div>
+          )}
     </div>
   );
 };
